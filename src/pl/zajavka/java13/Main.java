c8d44c8009f318e8ed34d983038a33b1250696f5
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2024-10-28 09:29:57 +0100

    added java 13 examples and notes

---
+++ src/pl/zajavka/java13/Main.java
@@ -0,0 +1,9 @@
+package pl.zajavka.java13;
+
+public class Main {
+    // switch - propozycja zmiany zapisu switcha (wszedl w wersji 14)
+    // mozliwosc pisania stringow jako wielolinijkowe bloki tekstu / wprowadzone w 15
+    String textBlock = """
+            
+            """;
+}
