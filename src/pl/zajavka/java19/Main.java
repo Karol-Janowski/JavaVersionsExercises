53669fda62b7e80696390666447375df082b973d
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2024-11-07 10:58:00 +0100

    added java 19 examples and notes

---
+++ src/pl/zajavka/java19/Main.java
@@ -0,0 +1,10 @@
+package pl.zajavka.java19;
+
+public class Main {
+    public static void main(String[] args) {
+        // virtual threads - preview feature
+        // pattern matching for switch - 3th preview feature
+        // record pattern = preview feature
+        // zmiana w locale - Locale.of() zamiast new Locale
+    }
+}
