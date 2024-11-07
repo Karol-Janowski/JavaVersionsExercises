7de27a5022585e8786efbd6286d298596689407b
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2024-11-07 11:02:47 +0100

    added java 20 examples and notes

---
+++ src/pl/zajavka/java20/Main.java
@@ -0,0 +1,9 @@
+package pl.zajavka.java20;
+
+public class Main {
+    public static void main(String[] args) {
+        // virtual threads - preview feature
+        // record patterns - preview feature
+        // pattern matching fr switch -4th preview feature
+    }
+}
