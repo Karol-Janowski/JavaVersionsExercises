22dae4e4922ba293e4c837603e8590d0768e4bd1
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2024-11-07 10:52:59 +0100

    added java 18 examples and notes

---
+++ src/pl/zajavka/java18/Main.java
@@ -0,0 +1,11 @@
+package pl.zajavka.java18;
+
+public class Main {
+    public static void main(String[] args) {
+        // pattern matching for switch - zostal utrzymany jako preview
+        // Deprecate Finalization for Removal
+        // metoda finalize - garbage collector - chcemy pozamykac zasoby
+        // nie ma gwarancji kiedy te sprzatanie jest wywolywane przez garbage collector
+
+    }
+}
